a495c4512e0e6349758d08c9ebce146ba3d4810d
Rumi Jahan Dalas <rumi@bd.batworld.com>
2023-05-09 11:11:47 +0600

    multi axis line chart v2

--- src/app/multi-axis-line-chart/multi-axis-line-chart.component.ts
+++ src/app/multi-axis-line-chart/multi-axis-line-chart.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 import { Chart, ChartConfiguration, ChartEvent, ChartType } from 'chart.js';
-import { BaseChartDirective } from 'ng2-charts'
 import { default as Annotation } from 'chartjs-plugin-annotation';
 
 @Component({
@@ -13,18 +12,17 @@ export class MultiAxisLineChartComponent {
    // private newLabel? = 'New label';
 
    constructor() {
-    Chart.register(Annotation)
+    // Chart.register(Annotation)
   }
 
   public lineChartData: ChartConfiguration['data'] = {
     datasets: [
 
       {
-        data: [ 239, 159, 595, 594, 102, 92, 156 , 623, 244],
-        label: 'Series A',
+        data: [ 123, 595, 594, 102, 92, 156 , 622, 487, 77],
+        label: 'Application',
         backgroundColor: '#3F7D20'+ '80',
-        // backgroundColor: 'rgba(148,159,177,0.2)',
-        borderColor: 'rgba(148,159,177,1)',
+        borderColor: '#3F7D20'+ '80' ,
         pointBackgroundColor: 'rgba(148,159,177,1)',
         pointBorderColor: '#fff',
         pointHoverBackgroundColor: '#fff',
@@ -32,13 +30,11 @@ export class MultiAxisLineChartComponent {
         fill: 'origin',
       },
       {
-        data: [ 271, 624, 676, 640, 391, 218, 340 , 704, 418],
-        label: 'Series B',
+        data: [ 507, 676, 636, 389,  217, 339 , 699, 740 , 107],
+        label: 'Disposed',
         backgroundColor: '#FAA916'+ '80',
-
-        // backgroundColor: 'rgba(77,83,96,0.2)',
-        borderColor: 'rgba(77,83,96,1)',
-        pointBackgroundColor: 'rgba(77,83,96,1)',
+        borderColor: '#FAA916' + '80',
+        pointBackgroundColor: 'red',
         pointBorderColor: '#fff',
         pointHoverBackgroundColor: '#fff',
         pointHoverBorderColor: 'rgba(77,83,96,1)',
@@ -47,7 +43,7 @@ export class MultiAxisLineChartComponent {
       },
 
     ],
-    labels: [ '30-Apr', '01-May', '02-May', '03-May', '04-May', '05-May', '06-May','07-May','08-May' ]
+    labels: [ '30-Apr', '01-May', '02-May', '03-May', '04-May', '05-May', '06-May','07-May','08-May'  ]
   };
 
   public lineChartOptions: ChartConfiguration['options'] = {
@@ -58,64 +54,21 @@ export class MultiAxisLineChartComponent {
     },
     scales: {
       // We use this empty structure as a placeholder for dynamic theming.
-
       y:
         {
           position: 'left',
         },
-      // y1: {
-      //   position: 'right',
-      //   grid: {
-      //     color: 'rgba(255,0,0,0.3)',
-      //   },
-      //   ticks: {
-      //     color: 'red'
-      //   }
-      // }
+
     },
 
     plugins: {
       legend: { display: true },
-      // annotation: {
-      //   annotations: [
-      //     {
-      //       type: 'line',
-      //       scaleID: 'x',
-      //       value: 'March',
-      //       borderColor: 'orange',
-      //       borderWidth: 2,
-      //       label: {
-      //         display: true,
-      //         position: 'center',
-      //         color: 'orange',
-      //         content: 'LineAnno',
-      //         font: {
-      //           weight: 'bold'
-      //         },
 
-      //       }
-      //     },
-      //   ],
-      // }
     }
   };
 
   public lineChartType: ChartType = 'line';
 
-  // @ViewChild(BaseChartDirective) chart?: BaseChartDirective;
-
-  // private static generateNumber(i: number): number {
-  //   return Math.floor((Math.random() * (i < 2 ? 100 : 1000)) + 1);
-  // }
-
-  // public randomize(): void {
-  //   for (let i = 0; i < this.lineChartData.datasets.length; i++) {
-  //     for (let j = 0; j < this.lineChartData.datasets[i].data.length; j++) {
-  //       this.lineChartData.datasets[i].data[j] = LineChartComponent.generateNumber(i);
-  //     }
-  //   }
-  //   this.chart?.update();
-  // }
 
   // events
   public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
